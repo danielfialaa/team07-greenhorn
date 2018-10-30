bb000c9a61aa8cd07cebbc29e0e33c059a89a612
danielfialaa <danielfialaa@gmail.com>
2018-10-30 17:42:04 +0000

    ??

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,15 +1,15 @@
 module.exports = {
   "development": {
-    "username": "team07",
-    "password": "xe7wd48swrfdi6vx",
-    "database": "user_team07",
+    "username": "root",
+    "password": "",
+    "database": "greenhorn",
     "host": "localhost",
     "dialect": "mysql"
   },
   "production": {
-    "username": "team07",
-    "password": "xe7wd48swrfdi6vx",
-    "database": "user_team07",
+    "username": "root",
+    "password": "",
+    "database": "greenhorn",
     "host": "localhost",
     "dialect": "mysql"
   }
