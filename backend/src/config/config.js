26f722c68b84a25eeeb2ae5d364c6c47e8c9f61c
danielfialaa <danielfialaa@gmail.com>
2018-10-30 17:12:08 +0000

    added config to github

---
+++ backend/src/config/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "development": {
+    "username": "team07",
+    "password": "xe7wd48swrfdi6vx",
+    "database": "user_team07",
+    "host": "localhost",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "team07",
+    "password": "xe7wd48swrfdi6vx",
+    "database": "user_team07",
+    "host": "localhost",
+    "dialect": "mysql"
+  }
+};
