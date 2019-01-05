e4bc6a781763d95521f4a37b432219acffe6f7ee
alexKolpakova <alex.k.kolpakova@gmail.com>
2019-01-05 16:47:37 +0100

    -notification for Reopened task added; -notification for newly assigned task added; -page reload after deleting task added;

--- backend/src/modules/assign-task/assignTaskController.js
+++ backend/src/modules/assign-task/assignTaskController.js
@@ -10,15 +10,12 @@ export const assignTaskController = async (req, res) => {
     })
     .then(response => {
       req.body.idReporter = response[0].id;
-      const form = db.task_history
-        .create(req.body)
-        // .then(response => {})
-        .catch(e => {
-          console.log('e:', e);
-          res.json({
-            status: false,
-          });
+      const form = db.task_history.create(req.body).catch(e => {
+        console.log('e:', e);
+        res.json({
+          status: false,
         });
+      });
       console.log('form' + form);
       console.log('response' + response);
     });
@@ -38,40 +35,68 @@ export const assignTaskController = async (req, res) => {
   });
 
   const notificationDate = moment(req.body.dateOfDeadline, 'YYYY-MM-DD')
-    .add(-6, 'days')
-    .add(-41, 'minutes')
+    .add(-1, 'days')
+    .add(9, 'hours')
     .toDate();
 
+  const taskSummaryMail = require('sendmail')();
+  console.log('notificationDate: ' + notificationDate);
+  taskSummaryMail(
+    {
+      from: 'no-reply@greenhorn.com',
+      to: user.email,
+      subject: 'New task assigned!',
+      html:
+        'Hi ' +
+        user.firstName +
+        ' ' +
+        user.lastName +
+        '! \n' +
+        'A new task "' +
+        task.name +
+        '" with deadline on [' +
+        req.body.dateOfDeadline +
+        '] was assigned to you. Please do not forget to close task in time! \n' +
+        'In case of any question visit GreenHorn application for detailed task description or contact by email task requestor - ' +
+        req.user.email +
+        '. ',
+    },
+    function(err, reply) {
+      console.log(err && err.stack);
+      console.dir(reply);
+    }
+  );
+
   const schedule = require('node-schedule');
-  const sch = schedule.scheduleJob(notificationDate, function() {
-    const sendmail = require('sendmail')();
-    console.log('date: ' + date);
-    console.log('notificationDate: ' + notificationDate);
-    console.log('notDate: ' + notDate);
-    sendmail(
-      {
-        from: 'no-reply@greenhorn.com',
-        to: user.email,
-        subject: 'Task is close to deadline period!',
-        html:
-          'Hi ' +
-          user.firstName +
-          ' ' +
-          user.lastName +
-          '! \n' +
-          'You have not finished task "' +
-          task.name +
-          '" which is close to defined deadline: ' +
-          req.body.dateOfDeadline +
-          '. Please do not forget to close task in time! \n' +
-          'In case of any question visit GreenHorn application for detailed task description or contact by email task requestor - ' +
-          req.user.email +
-          '. ',
-      },
-      function(err, reply) {
-        console.log(err && err.stack);
-        console.dir(reply);
-      }
-    );
-  });
+  const notifyOneDayBeforeDeadline = schedule.scheduleJob(
+    notificationDate,
+    function() {
+      const sendmail = require('sendmail')();
+      sendmail(
+        {
+          from: 'no-reply@greenhorn.com',
+          to: user.email,
+          subject: 'Task is close to deadline period!',
+          html:
+            'Hi ' +
+            user.firstName +
+            ' ' +
+            user.lastName +
+            '! \n' +
+            'Your task "' +
+            task.name +
+            '" is close to defined deadline: ' +
+            req.body.dateOfDeadline +
+            '. Verify your task is completed and closed! \n' +
+            'In case of any question visit GreenHorn application for detailed task description or contact by email task requestor - ' +
+            req.user.email +
+            '. ',
+        },
+        function(err, reply) {
+          console.log(err && err.stack);
+          console.dir(reply);
+        }
+      );
+    }
+  );
 };
--- backend/src/modules/modify-user-task/modifyUserTaskController.js
+++ backend/src/modules/modify-user-task/modifyUserTaskController.js
@@ -1,49 +1,121 @@
 import db from '../../models/';
+import moment from 'moment';
 
 export const modifyUserTaskController = async (req, res) => {
   const data = await req.body;
 
-  if(req.body.status == 'DONE') {
-    const result = db.task_history.update(
-      {
-        status: req.body.status
-      },
-      {
-        where: {
-          id: req.body.id
+  if (req.body.status == 'DONE') {
+    const result = db.task_history
+      .update(
+        {
+          status: req.body.status,
+        },
+        {
+          where: {
+            id: req.body.id,
+          },
         }
-      }
-    ).then(response => {
-      // response = JSON.stringify(response)
-      // response = JSON.parse(response)
-      res.json({
-        response,
+      )
+      .then(response => {
+        // response = JSON.stringify(response)
+        // response = JSON.parse(response)
+        res.json({
+          response,
+        });
+      })
+      .catch(error => {
+        console.log(error);
       });
-    }).catch(error => {
-      console.log(error);
-    });
-}
+  }
 
-  if(req.body.status != 'DONE') {
-  const result = db.task_history
-    .update(
-      {
-        dateOfCompletion: req.body.dateOfCompletion,
-        status: req.body.status
-      },
-      {
-      where: {
-        id: req.body.id
-      }
-    })
-    .then(response => {
-      // response = JSON.stringify(response)
-      // response = JSON.parse(response)
-      res.json({
-        response,
+  if (req.body.status == 'TBD') {
+    const result = db.task_history
+      .update(
+        {
+          dateOfCompletion: req.body.dateOfCompletion,
+          status: req.body.status,
+        },
+        {
+          where: {
+            id: req.body.id,
+          },
+        }
+      )
+      .then(response => {})
+      .catch(error => {
+        console.log(error);
       });
-    }).catch(error => {
-      console.log(error);
+
+    const taskHistory = await db.task_history.findOne({
+      attributes: ['idTask', 'idUser', 'dateOfDeadline'],
+      where: { id: req.body.id },
     });
+
+    const task = await db.tasks.findOne({
+      attributes: ['name'],
+      where: { id: taskHistory.idTask },
+    });
+
+    const user = await db.users.findOne({
+      attributes: ['firstName', 'lastName', 'email'],
+      where: { id: taskHistory.idUser },
+    });
+
+    res.json({
+      status: true,
+      result,
+      task,
+      taskHistory,
+      user,
+    });
+
+    const sendmail = require('sendmail')();
+    sendmail(
+      {
+        from: 'no-reply@greenhorn.com',
+        to: user.email,
+        subject: 'Task status changed',
+        html:
+          'Hi ' +
+          user.firstName +
+          ' ' +
+          user.lastName +
+          '! \n' +
+          'Your task "' +
+          task.name +
+          '" status was changed to - ' +
+          req.body.status +
+          ' - TO BE DONE, with deadline date set to [' +
+          moment(taskHistory.dateOfDeadline).format('YYYY-MM-DD') +
+          ']. For futher information visit GreenHorn application.',
+      },
+      function(err, reply) {
+        console.log(err && err.stack);
+        console.dir(reply);
+      }
+    );
+  }
+
+  if (req.body.status != 'TBD' && req.body.status != 'DONE') {
+    const result = db.task_history
+      .update(
+        {
+          dateOfCompletion: req.body.dateOfCompletion,
+          status: req.body.status,
+        },
+        {
+          where: {
+            id: req.body.id,
+          },
+        }
+      )
+      .then(response => {
+        res.json({
+          response,
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 };
