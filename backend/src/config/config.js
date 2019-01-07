0676259a1e2843007153354cb876b4c700bc5455
unknown <danielfialaa@gmail.com>
2019-01-07 17:46:21 +0100

    package.json added + config.js added

---
+++ backend/src/config/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "development": {
+    "username": USERNAME,
+    "password": PASSWORD,
+    "database": DATABASE_NAME,
+    "host": DATABASE_HOST,
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": USERNAME,
+    "password": PASSWORD,
+    "database": DATABASE_NAME,
+    "host": DATABASE_HOST,
+    "dialect": "mysql"
+  }
+};
